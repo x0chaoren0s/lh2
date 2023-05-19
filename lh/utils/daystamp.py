74f81aac072b0948d1d75520873c417b768dfdac
x0chaoren0s <x0chaoren0s@gmail.com>
2023-05-19 11:27:30 +0800

    update_inday_deals

--- lh/utils/daystamp.py
+++ lh/utils/daystamp.py
@@ -121,10 +121,6 @@ class DayStamp:
             except:
                 assert False, f'{ts_code}, {self.trade_date}'
 
-
-            
-
-
     def _try_sell(self, ts_code, sell_vol, sell_price, fenshi_startind:int=None):
         '''
         ①校正【卖出量】至多为昨日持有量，【卖出价】为调整单次交易佣金、印花税、过户费后的价格
@@ -162,5 +158,67 @@ class DayStamp:
             # if self.end_tscode2vol_dict[ts_code]==0:   # 不再交易完后就删除清仓股票，而是转到所有交易都做完后统一处理
             #     del self.end_tscode2vol_dict[ts_code]
 
+    def update_inday_deals(self, sell_form_dict, buy_form_dict):
+        '''
+        sell_form_dict = {
+            'sell_tscode_list' : flask.request.form.getlist('sell_tscode'),
+            'sell_type_list' : [flask.request.form.get(f'sell_type_{tscode}') for tscode in flask.request.form.getlist('sell_tscode')]
+        }
+            <input type="radio" name="sell_type_{{stk.ts_code}}" value="no" checked="checked">不清仓<br>
+            <input type="radio" name="sell_type_{{stk.ts_code}}" value="hongpanqingcang">红盘清仓<br>
+            <input type="radio" name="sell_type_{{stk.ts_code}}" value="shuishangqingcang">水上清仓<br>
+            <input type="radio" name="sell_type_{{stk.ts_code}}" value="dietingqingcang">跌停清仓
+        buy_form_dict = {
+            'buy_tscode_list' : flask.request.form.getlist('buy_tscode'),
+            'buy_price_list' : [float(p) for p in flask.request.form.getlist('buy_price')],
+            'buy_hands_list' : [int(h) for h in flask.request.form.getlist('buy_hands')]
+        }
 
+        fenshi_startind: 日内自动止盈挂单，从该分时数据ind（0~239）开始挂分时单交易。默认为None全天都挂单
+
+        仅处理日内交易，先处理卖单，再处理买单
+        '''
+        self.sell_list = sell_form_dict['sell_tscode_list']
+        for ts_code, sell_price in zip(sell_form_dict['sell_tscode_list'],sell_form_dict['sell_price_list']):
+            # 昨日尾盘买入的今日上午卖出
+            sell_price = float(sell_price)
+            if sell_price == 0.0:
+                continue
+            if self.tdf.daily(ts_code, self.trade_date).vol.iloc[0]==0:
+                continue
+            sell_vol = self.start_tscode2vol_dict[ts_code]
+            sellvalue = sell_vol * sell_price
+            prevalue = sell_vol * self.ssd.getPreClose(ts_code, self.trade_date)
+            self.end_cash += sellvalue-5-round(sellvalue/1000,2)
+            self.end_cash -= round(sellvalue/1000)/100 if ts_code.startswith('60') else 0
+            self.end_total = self.end_total-prevalue+sellvalue-5-round(sellvalue/1000,2)
+            self.end_total -= round(sellvalue/1000)/100 if ts_code.startswith('60') else 0
+            self.end_tscode2vol_dict[ts_code] -= sell_vol
+        self.buy_list = buy_form_dict['buy_tscode_list']
+        for ts_code, hands, price in zip(self.buy_list, buy_form_dict['buy_hands_list'], buy_form_dict['buy_price_list']):
+            # 今日尾盘买入今日尾盘跌停的
+            if hands==0 or price==0.0:
+                continue
+            daily_df = self.ssd.getDaily_df(ts_code=ts_code,trade_date=self.trade_date)
+            if len(daily_df)==0 or daily_df.vol.iloc[0]==0:
+                continue
+            assert len(daily_df)>0, f'{ts_code}, {self.trade_date}'
+            if price<daily_df.low.iloc[0]:
+                continue
+            spend_value = hands*100*price+5
+            spend_value += round(spend_value/1000)/100 if ts_code.startswith('60') else 0
+            if spend_value >= self.end_cash:
+                continue
+            self.end_tscode2vol_dict.setdefault(ts_code, 0)
+            self.end_tscode2vol_dict[ts_code] += hands*100
+            end_value = hands*100*self.ssd.getDaily_df(ts_code=ts_code,trade_date=self.trade_date).close.iloc[0]
+            self.end_cash -= spend_value
+            assert self.end_cash>0
+            self.end_total = self.end_total-spend_value+end_value
+        for ts_code, vol in list(self.end_tscode2vol_dict.items()):
+            if vol==0 and ts_code in self.start_tscode2vol_dict and self.start_tscode2vol_dict[ts_code]==0:
+                del self.end_tscode2vol_dict[ts_code]
+            if vol==0:
+                continue
+        
         
